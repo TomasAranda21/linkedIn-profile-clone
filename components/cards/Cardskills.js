0dc93212d5c17f1dec785aaef1d635d65d44666f
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-26 18:53:34 -0300

    add skills section with your card

--- components/cards/Cardskills.js
+++ components/cards/Cardskills.js
@@ -1,22 +1,46 @@
 import React from 'react'
+import icons from '../icons/icons'
 
-const Cardskills = () => {
+const Cardskills = ({type, displayButton, isNotValidated, title}) => {
   return (
-    <div>
-        <h3>HTML5</h3>
+    <div className={` text-sm ${ type ? 'border-nome': "border-b border-white border-opacity-20 pb-6" }`}>
 
-        <div>
-            <div>
+        <h3 className="font-bold mb-3">{title}</h3>
+
+        <div className="flex flex-col gap-4 lg:pl-1 pl-0">
+            <div className="flex items-center gap-2">
+                <p className="text-xl text-blue-300">{icons.imgLinkedinIcon}</p>
                 <p>Desarrollador web en Verdulería Luz | Peluquería Marga</p>
             </div>
 
-            <div>
-                2 validaciones2
-            </div>
 
-            <div>
+            {!isNotValidated ? 
+            <div className="flex items-center gap-2">
+                <p className="text-xl">{icons.userIcon}</p>
+                <p>2 validaciones</p>
+            </div>
+            : 
+            <div className="flex items-center gap-2">
+                <p className="text-xl">{icons.checkIcon}</p>
+                <p>Ha superado la evaluación de aptitudes de LinkedIn</p>
+            </div>
+            
+            }
+
+            
+
+            <div className="flex items-center gap-2">
+            <p className="text-xl">{icons.folderIcon}</p>
+
                 <p>Clon de Netflix</p>
             </div>
+
+            {displayButton && 
+                <div className="flex items-center gap-2 font-semibold cursor-pointer hover:bg-white hover:bg-opacity-20 p-1 px-2 w-2/3 md:w-1/3 md:justify-center rounded-lg">
+                    <p>Mostrar todos los datos (4)</p>
+                    <p>{icons.arrowRightIcon}</p>
+                </div>
+            }
         </div>
     </div>
   )
@@ -24,7 +48,5 @@ const Cardskills = () => {
 
 export default Cardskills
 
-
-HTML5HTML5
 
 
--- components/sections/SectionSkills.js
+++ components/sections/SectionSkills.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Cardskills from '../cards/Cardskills'
 import ContainerViewMore from '../containers/ContainerViewMore'
 
 
@@ -7,7 +8,14 @@ const SectionSkills = () => {
 
     <ContainerViewMore title="Conocimientos y aptitudes" textSeeMore="Mostrar todas las aptitudes (18)">
         
+      <div className="flex flex-col gap-3 mt-5">
+        
+        <Cardskills title="HTML5"/>
+        <Cardskills displayButton={true} isNotValidated={true} title="React.js"/>
+        <Cardskills type={true} title="JavaScript"/>
 
+   
+      </div>
 
     </ContainerViewMore>
 
