f4c1d63d490caddfea680a2afe095e6179a77f06
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-26 20:19:26 -0300

    add improvements for responsive

--- components/cards/CardActivity.js
+++ components/cards/CardActivity.js
@@ -10,7 +10,7 @@ const CardActivity = ({img, title, text, comments, likes, date, author, border})
     <h3 className="mt-3 text-xs text-gray-200 text-opacity-70"><span className="font-bold">Tomás Aranda</span> ha publicado esto • {date}</h3>
         <p className="text-sm">{title}</p>
         <div className="flex gap-3 items-start pb-4">
-            <div className="overflow-hidden rounded-lg">
+            <div className="overflow-hidden md:rounded-lg w-full sm:w-auto">
                 <img className=" object-none h-20" 
                 src={img} />
             </div>
