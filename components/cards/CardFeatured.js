538b6138b983aa121d6f84eea44d874db4c7ae1d
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-27 20:22:20 -0300

    fix size card error

--- components/cards/CardFeatured.js
+++ components/cards/CardFeatured.js
@@ -3,7 +3,10 @@ import CommentsAndLikes from '../commentsAndLikes/CommentsAndLikes'
 const CardFeatured = ({img, text, type, text2, comments, likes}) => {
   return (
 
-    <div className=" w-full sm:w-56 sm:h-60 overflow-hidden border rounded-xl gap-4 border-gray-500 border-opacity-60 flex md:block flex-col justify-between pb-5">
+    <div className="w-full 
+    sm:w-56 sm:h-60 lg:w-full overflow-hidden border rounded-xl gap-4 
+
+  border-gray-500 border-opacity-60 flex md:block flex-col justify-between pb-5">
 
         <p className="text-gray-200 text-opacity-70 text-sm p-2">{type}</p>
 
