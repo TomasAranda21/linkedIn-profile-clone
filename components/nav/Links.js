9baff0d1b7ce0f2d113b689176b0223a32447294
Tomas Aranda <tomasaranda2018@gmail.com>
2022-07-27 20:35:15 -0300

    fix size to text error

--- components/nav/Links.js
+++ components/nav/Links.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Links = ({text, icon}) => {
   return (
-    <div className="flex flex-col gap-0.5 items-center text-opacity-80 text-gray-100 hover:text-opacity-100 
+    <div className="flex flex-col gap-0.5 items-center text-opacity-80 text-gray-100 hover:text-opacity-100 w-full
     hover:text-white cursor-pointer duration-200">
         <p className="text-xl">{icon}</p>
-        <p className="">{text}</p>
+        <p className="text-xs">{text}</p>
     </div>
   )
 }
